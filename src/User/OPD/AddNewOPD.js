ae5d3c82c5c7329a1566e7eee585f4c114a122ea
Romain Perera <chrish.romi16@gmail.com>
2022-06-14 17:16:26 +0530

    new changes

--- src/User/OPD/AddNewOPD.js
+++ src/User/OPD/AddNewOPD.js
@@ -4,59 +4,105 @@ import {
   Text,
   View,
   Button,
-  Keyboard,
-  TouchableWithoutFeedback
+  TouchableOpacity,
 } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native'; 
 
 export default function AddNewE_Claim(){
 
-  const DissmissKeyboard = ({children}) =>(
-    <TouchableWithoutFeedback onPress={()=>Keyboard.dismiss()}>
-      {children}
-    </TouchableWithoutFeedback>
-  );
+  // const DissmissKeyboard = ({children}) =>(
+  //   <TouchableWithoutFeedback onPress={()=>Keyboard.dismiss()}>
+  //     {children}
+  //   </TouchableWithoutFeedback>
+  // );
 
   const [description,setDescriptiont] = useState("");
-  const [ receiptNo,setReceiptNo]=useState("");
-  const [ amount,setAmount]=useState("");
+  const [ receipt_no,setReceiptNo]=useState("");
+  const [ opd_amount,setAmount]=useState("");
 
   const navigation=useNavigation();
 
-  const next = () => {
-    navigation.navigate("View the new OPD",{
-      description:description,
-      receiptNo:receiptNo,
-      amount:amount,
-    })
-  }
+  // const next = () => {
+  //   navigation.navigate("View the new OPD",{
+  //     description:description,
+  //     receiptNo:receiptNo,
+  //     amount:amount,
+  //   })
+  // }
+  async function claimOPD() {
+    if(isNaN(receipt_no),(opd_amount)){
+      alert(" Not a number");
+      //this.setState({ email: text })
+      return false;
+      // Its not a number
+    }
+
+  try {
+    fetch('http://10.0.2.2:8080/api/v2/opd/saveOPD', {
+   method: 'POST',
+   headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+            },
+   body: JSON.stringify({
+      description,
+      receipt_no,
+      opd_amount,
+  })
+
+})
+
+     alert('OPD claimed successfully')
+        navigation.navigate('OPD' )
+      } catch (error) {
+           console.error(error);
+      }
+
+}
 
     return(
-      <DissmissKeyboard>
       <View style={styles.body}>
-      <TextInput style={styles.input}
+        <View style={styles.header}>
+        <Text style={styles.text}>
+        Add a new OPD
+        </Text>
+        </View>
+        <View style={styles.container}>
+
+        <TextInput  onChangeText={newText => setDescriptiont(newText)} style={styles.input}
         placeholder='Description'
         value={description}
-        onChangeText={(text)=>setDescriptiont(text)}
         />
-        <TextInput style={styles.input}
-        placeholder='Receipt Not'
-        value={receiptNo}
-        onChangeText={(text)=>setReceiptNo(text)}
+        {/* {!!this.state.nameError && (
+        <Text style={{ color: "red" }}>{this.state.nameError}</Text>
+        )} */}
+        
+        <TextInput  onChangeText={newText => setReceiptNo(newText)} style={styles.input}
+        placeholder='Receipt No'
+        keyboardType='numeric'
+        value={receipt_no}
         />
-         <TextInput style={styles.input}
+        <TextInput  onChangeText={newText => setAmount(newText)} style={styles.input}
         placeholder='Amount'
-        value={amount}
-        onChangeText={(text)=>setAmount(text)}
+        value={opd_amount}
+        keyboardType='numeric'
         />
-        <Button style={styles.button}
-         title='Next'
-         onPress={next}
-         color="#e63909"
-        />
+        <TouchableOpacity style={styles.background}
+          onPress={claimOPD} >
+          {/* // onPress={() => {
+          //   if (this.state.text.trim() === "") {
+          //     this.setState(() => ({ nameError: "First name required."}));
+          //   } else {
+          //     this.setState(() => ({ nameError: null}));
+          //   }
+          // }}> */}
+        <Text style={styles.textB}>
+          Submit
+        </Text>
+        </TouchableOpacity>
       </View>
-      </DissmissKeyboard>
+      </View>
     );
   }
 
@@ -64,9 +110,57 @@ export default function AddNewE_Claim(){
   
     input:{
       height: 40,
-      margin: 12,
-      borderWidth: 1,
+      width:200,
+      margin: 5,
       padding: 10,
+      borderRadius:10,
+      marginLeft: '25%',
+      marginBottom: 10,
+      backgroundColor:'#ffffff',
+    },
+    background:{
+      backgroundColor: '#ffffff',
+      marginBottom:20,
+      marginTop:40,
+      // margin:0,
+      marginLeft: '25%',
+      width:'50%',
+      height: '10%',
+      justifyContent:'center',
+      alignItems:'center',
+      borderRadius:60,
+    },
+    textB:{
+      fontSize:16,
+      marginBottom: 10,
+      marginTop: 1,
+      marginStart:1,
+      // textAlign:'left',
+      marginLeft:10,
+      color:'#000000',
+      fontWeight:'bold',
+    },
+    body:{
+      flex:1,
+      // justifyContent:'center',
+      // alignItems:'center',
+      backgroundColor:'#ffffff',
+    },
+    header:{
+      flex:1,
+      backgroundColor:'#ffffff',
+    },
+    text:{
+      fontSize:30,
+      fontWeight:'bold',
+      margin:30,
+      color:'#000000',
+    },
+    container:{
+      flex:5,
+      backgroundColor:'#F89880',
+      borderTopLeftRadius: 60,
+      borderTopRightRadius: 60,
     },
   })
   
--- src/User/OPD/AddNewOPD2.js
+++ src/User/OPD/AddNewOPD2.js
@@ -5,40 +5,137 @@ import {
   Text,
   View,
   Button,
-  Image,
-  Pressable,
+  TouchableOpacity,
 } from 'react-native';
-
-import { useRoute } from '@react-navigation/native';
-
+import { TextInput } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native'; 
 
 export default function Add_OPD2(){
-  const route= useRoute();
+  const [description,setDescriptiont] = useState("");
+  const [ receipt_no,setReceiptNo]=useState("");
+  const [ opd_amount,setAmount]=useState("");
+
+  const navigation=useNavigation();
+
+  async function updateOPD() {
+    if(isNaN(receipt_no)){
+      alert(" Not a number");
+      //this.setState({ email: text })
+      return false;
+      // Its not a number
+    }
+
+  try {
+    fetch('http://10.0.2.2:8080/api/v2/opd/updateOPD', {
+   method: 'PUT',
+   headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+            },
+   body: JSON.stringify({
+      description,
+      receipt_no,
+      opd_amount,
+  })
+
+})
+
+     alert('OPD claimed successfully')
+        navigation.navigate('OPD' )
+      } catch (error) {
+           console.error(error);
+      }
+
+}
+
+
     return(
       <View style={styles.body}>
-        <Text style={styles.text}>Description: {route.params.description}</Text>
-        <Text style={styles.text}>Receipt No: {route.params.receiptNo}</Text>
-        <Text style={styles.text}>Amount: {route.params.amount}</Text>
-
-        <Button style={styles.button}
-         title='Submit'
-         color="#e63909"
-        />
+      <View style={styles.header}>
+      <Text style={styles.text}>
+      Edit OPD
+      </Text>
       </View>
-    )
-  }
+      <View style={styles.container}>
 
-  const styles = StyleSheet.create({
-    body:{
-      flex:1,
-      justifyContent:'center',
-      alignItems:'center',
-    },
-    input1:{
-      fontSize:20,
-    },
-    text:{
-      fontSize:15,
-    }
-  })
-  
+      <TextInput  onChangeText={newText => setDescriptiont(newText)} style={styles.input}
+      placeholder='Description'
+      value={description}
+      />
+      <TextInput  onChangeText={newText => setReceiptNo(newText)} style={styles.input}
+      placeholder='Receipt No'
+      value={receipt_no}
+      />
+      <TextInput  onChangeText={newText => setAmount(newText)} style={styles.input}
+      placeholder='Amount'
+      value={opd_amount}
+      />
+      <TouchableOpacity style={styles.background}
+        onPress={updateOPD}>
+      <Text style={styles.textB}>
+        Submit
+      </Text>
+      </TouchableOpacity>
+    </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+  input:{
+    height: 40,
+    width:200,
+    margin: 5,
+    borderWidth: 1,
+    padding: 10,
+    // marginHorizontal:150,
+    marginLeft: '25%',
+    marginBottom: 10,
+    backgroundColor:'#ffffff',
+  },
+  background:{
+    backgroundColor: '#ffffff',
+    marginBottom:20,
+    marginTop:40,
+    // margin:0,
+    marginLeft: '25%',
+    width:'50%',
+    height: '10%',
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:60,
+  },
+  textB:{
+    fontSize:16,
+    marginBottom: 10,
+    marginTop: 1,
+    marginStart:1,
+    // textAlign:'left',
+    marginLeft:10,
+    color:'#000000',
+    fontWeight:'bold',
+  },
+  body:{
+    flex:1,
+    // justifyContent:'center',
+    // alignItems:'center',
+    backgroundColor:'#ffffff',
+  },
+  header:{
+    flex:1,
+    backgroundColor:'#ffffff',
+  },
+  text:{
+    fontSize:30,
+    fontWeight:'bold',
+    margin:30,
+    color:'#000000',
+  },
+  container:{
+    flex:5,
+    backgroundColor:'#F89880',
+    borderTopLeftRadius: 60,
+    borderTopRightRadius: 60,
+  },
+})
